596a7f2f4ee2a069e296da2ae231f5c3401591b2
Tree <56370416+Tojo-hung@users.noreply.github.com>
2021-05-19 07:27:13 -0700

    Update stopwatch.ino

--- stopwatch.ino
+++ stopwatch.ino
@@ -1,15 +1,24 @@
 
 #include <LiquidCrystal.h>
 
+#include <EEPROM.h>
+
 const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+struct Savetime {
+  int minutesS;
+  float secondsS;
+};
 
-long seconds;
+float seconds;
 long mil;
 int minutes = 0;
-long time;
+float time;
 int sub;
+float a = millis();
+float c;
+int eeAddress = 0;
 
 void setup() {
   lcd.begin(16, 2);
@@ -21,9 +30,11 @@ void setup() {
   digitalWrite(8, HIGH);
   pinMode(9, INPUT);
   digitalWrite(9, HIGH);
+  pinMode(10, INPUT);
+  digitalWrite(10, HIGH);
+  pinMode(11, INPUT);
+  digitalWrite(11, HIGH);
 }
-float a = millis();
-float c;
 
 void loop() {
   minutes = 0;
@@ -32,8 +43,26 @@ void loop() {
   time = 0;
   sub = 0;
   lcd.clear();
-  lcd.print("press start");
-  delay(100);
+  lcd.print("press start/read");
+  if (digitalRead(11) == LOW) {
+    while (digitalRead(8) == HIGH) {
+      Savetime customVar;
+      EEPROM.get(eeAddress, customVar);
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Saved Time:");
+      lcd.setCursor(0, 1);
+      if (customVar.minutesS < 10) {
+        lcd.print("0");
+      }
+      lcd.print(customVar.minutesS);
+      lcd.print(":");
+      if (customVar.secondsS < 10) {
+        lcd.print("0");
+      }
+      lcd.print(customVar.secondsS);
+    }
+  }
 
   if (digitalRead(8) == LOW) {
 
@@ -42,52 +71,66 @@ void loop() {
     while (digitalRead(9) == HIGH) {
 
       c = millis();
-      time = (c - a);
-
-      mil = time;
-
-      while (mil > 1000) {
-        mil = mil - 1000;
-      };
+      time = (c - a) / 1000;
 
 
-      seconds = (time - mil)/1000 - sub;
-      if (seconds > 59) {
+
+      seconds = time - sub;
+      if (seconds > 60) {
         sub = sub + 60;
         minutes++;
       }
 
-      if (minutes < 10){
+      if (minutes < 10) {
         lcd.print("0");
       }
       lcd.print(minutes);
       lcd.print(":");
-      if (seconds < 10){
-          lcd.print("0");
-        }
+      if (seconds < 10) {
+        lcd.print("0");
+      }
       lcd.print(seconds);
-      lcd.print(".");
-      lcd.print(mil);
       lcd.setCursor(0, 0);
       delay(100);
     }
 
     if (digitalRead(9) == LOW) {
       while (digitalRead(8) == HIGH) {
-        if (minutes < 10){
+        lcd.clear();
+        if (minutes < 10) {
           lcd.print("0");
         }
         lcd.print(minutes);
         lcd.print(":");
-        if (seconds < 10){
+        if (seconds < 10) {
           lcd.print("0");
         }
         lcd.print(seconds);
-        lcd.print(".");
-        lcd.print(mil);
-        lcd.setCursor(0, 0);
-        delay(100);
+        lcd.setCursor(0, 1);
+        lcd.print("Save, Yes or NO: ");
+        if (digitalRead(10) == LOW) {
+          while (digitalRead(8) == HIGH){
+            lcd.clear();
+            lcd.setCursor(0, 0);
+            lcd.print("Yes");
+            lcd.setCursor(0, 1);
+            if (minutes < 10) {
+                lcd.print("0");
+            }
+            lcd.print(minutes);
+            lcd.print(":");
+            if (seconds < 10) {
+              lcd.print("0");
+            }
+            lcd.print(seconds);
+            Savetime customVar = {
+              minutes,
+              seconds,
+            };
+            EEPROM.put(eeAddress, customVar);
+          }
+        }
       }
-}
-}
+    }
+  }
 }
